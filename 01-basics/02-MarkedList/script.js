fce6e0503ba9db452e87946032541c6e87411166
dinolike030 <egor.melnichenko@bk.ru>
2021-03-19 11:33:26 +0300

    implement marked list

--- 01-basics/02-MarkedList/script.js
+++ 01-basics/02-MarkedList/script.js
@@ -1,34 +1,64 @@
-// import Vue from './vendor/vue.esm.browser.js';
+import Vue from './vendor/vue.esm.browser.js';
 
 // From https://jsonplaceholder.typicode.com/comments
-/*
-const emails = [
-  'Eliseo@gardner.biz',
-  'Jayne_Kuhic@sydney.com',
-  'Nikita@garfield.biz',
-  'Lew@alysha.tv',
-  'Hayden@althea.biz',
-  'Presley.Mueller@myrl.com',
-  'Dallas@ole.me',
-  'Mallory_Kunze@marie.org',
-  'Meghan_Littel@rene.us',
-  'Carmen_Keeling@caroline.name',
-  'Veronica_Goodwin@timmothy.net',
-  'Oswald.Vandervort@leanne.org',
-  'Kariane@jadyn.tv',
-  'Nathan@solon.io',
-  'Maynard.Hodkiewicz@roberta.com',
-  'Christine@ayana.info',
-  'Preston_Hudson@blaise.tv',
-  'Vincenza_Klocko@albertha.name',
-  'Madelynn.Gorczany@darion.biz',
-  'Mariana_Orn@preston.org',
-  'Noemie@marques.me',
-  'Khalil@emile.co.uk',
-  'Sophia@arianna.co.uk',
-  'Jeffery@juwan.us',
-  'Isaias_Kuhic@jarrett.net',
-];
-*/
 
-// new Vue();
+// const emails = [
+//   'Eliseo@gardner.biz',
+//   'Jayne_Kuhic@sydney.com',
+//   'Nikita@garfield.biz',
+//   'Lew@alysha.tv',
+//   'Hayden@althea.biz',
+//   'Presley.Mueller@myrl.com',
+//   'Dallas@ole.me',
+//   'Mallory_Kunze@marie.org',
+//   'Meghan_Littel@rene.us',
+//   'Carmen_Keeling@caroline.name',
+//   'Veronica_Goodwin@timmothy.net',
+//   'Oswald.Vandervort@leanne.org',
+//   'Kariane@jadyn.tv',
+//   'Nathan@solon.io',
+//   'Maynard.Hodkiewicz@roberta.com',
+//   'Christine@ayana.info',
+//   'Preston_Hudson@blaise.tv',
+//   'Vincenza_Klocko@albertha.name',
+//   'Madelynn.Gorczany@darion.biz',
+//   'Mariana_Orn@preston.org',
+//   'Noemie@marques.me',
+//   'Khalil@emile.co.uk',
+//   'Sophia@arianna.co.uk',
+//   'Jeffery@juwan.us',
+//   'Isaias_Kuhic@jarrett.net',
+// ];
+
+
+new Vue({
+  data() {
+    return {
+      rawEmails: null,
+      searchQuery: '',
+    }
+  },
+  computed: {
+    emails() {
+      return this.searchQuery ? 
+      this.rawEmails?.map(email => ({
+        ...email,
+        matchQuery: email.address.toUpperCase().includes(this.searchQuery.toUpperCase()),
+      }))
+      :
+      this.rawEmails
+    } 
+      
+  },
+  created() {
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(res => res.json())
+      .then(postList => {
+        this.rawEmails = postList.map(post => ({
+          address: post.email,
+          matchQuery: false
+        }))
+      })
+  },
+  el: '#app',
+});
