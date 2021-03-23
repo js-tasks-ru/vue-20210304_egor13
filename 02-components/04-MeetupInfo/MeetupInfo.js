0b61dca2a04a602904a70b05d94f36888ce50b93
dinolike030 <egor.melnichenko@bk.ru>
2021-03-23 21:13:11 +0300

    solve 02-components task 4

--- 02-components/04-MeetupInfo/MeetupInfo.js
+++ 02-components/04-MeetupInfo/MeetupInfo.js
@@ -1,19 +1,46 @@
 export default {
   name: 'MeetupInfo',
+  props: {
+    place: {
+      type: String,
+      required: true,
+    },
+    organizer: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+      required: true,
+    }
+  },
+  computed: {
+    dateOnlyString() {
+      console.log(this.date);
+      return this.date.toISOString().split('T')[0];
+    },
+    localDate() {
+      return this.date.toLocaleString(navigator.language, {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+      });
+    }
+  },
 
   template: `
     <ul class="info-list">
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-user.svg" />
-        Организатор
+        {{ organizer }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-map.svg" />
-        Место
+        {{ place }}
       </li>
       <li>
         <img class="icon info-list__icon" alt="icon" src="/assets/icons/icon-cal-lg.svg" />
-        <time datetime="2020-01-01">1 янв. 2020</time>
+        <time :datetime="dateOnlyString">{{ localDate }}</time>
       </li>
     </ul>`,
 };
