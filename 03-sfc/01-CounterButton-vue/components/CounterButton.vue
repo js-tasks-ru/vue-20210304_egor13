5d46e508d60bd7706353706a3ce4f9bbbfe597d4
Egor Melnichenko <e.melnichenko@ibrush.ru>
2021-03-30 08:14:38 +0300

    03-sfc task 1

--- 03-sfc/01-CounterButton-vue/components/CounterButton.vue
+++ 03-sfc/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,26 @@
-<template><div></div></template>
+<template>
+    <button type="button" @click="handleClick">{{ count }}</button>
+</template>
 
 <script>
-export default {};
+export default {
+    name: 'CounterButton',
+    props: {
+        count: {
+            type: Number,
+            default: 0,
+        }
+    },
+    model: {
+        prop: 'count',
+        event: 'increment',
+    },
+    methods: {
+        handleClick() {
+            this.$emit('increment', this.count + 1)
+        }
+    }
+};
 </script>
 
 <style></style>
